29be131a1fa9fd147e7ba55b58cd346a9d632547
akshatsrf <akshatsrf860@gmail.com>
2022-08-17 18:39:27 +0530

    changed

--- fib.py
+++ fib.py
@@ -3,15 +3,15 @@ from tasks import fib,app1
 from celery.result import AsyncResult
 app = FastAPI()
 
-@app.post('/{number}')
+@app.get('/{number}')
 async def send_number(number: int):
     res = fib.delay(number)
-    return res.id
+    return res.get()
 
-@app.get('/result')
-async def get_fib(id):
-    result = AsyncResult(id, app=app1)
-    return result.get()
+# @app.get('/result')
+# async def get_fib(id):
+#     result = AsyncResult(id, app=app1)
+#     return result.get()
 
 @app.get('/without_celery/{number}')
 async def send_number_without_celery(number: int):
